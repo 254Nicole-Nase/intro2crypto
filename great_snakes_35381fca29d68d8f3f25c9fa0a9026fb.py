285c286215ac6b2f84b00412b0955f8b7d589843
Nicole Nasengo <nasengonicole@gmail.com>
2024-09-05 16:09:18 +0300

    bytes and integers to messages

--- great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
+++ great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-import sys
+
 import base64
+from Crypto.Util.number import *
+
 # import this
-if sys.version_info.major == 2:
-    print("You are running Python 2, which is no longer supported. Please update to Python 3.")
 
 char = [99, 114, 121, 112, 116, 111, 123, 65, 83, 67, 73, 73, 95, 112, 114, 49, 110, 116, 52, 98, 108, 51, 125]
 
@@ -17,3 +17,7 @@ flag3 = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
 flag3_1 = bytes.fromhex(flag3)
 print("your flag is: " + base64.b64encode(flag3_1).decode())
 
+# convert our messages into numbers so that mathematical operations can be applied
+flag4 = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+flag4_1 =  long_to_bytes(flag4)
+print("The flag is: " + flag4_1.decode())
