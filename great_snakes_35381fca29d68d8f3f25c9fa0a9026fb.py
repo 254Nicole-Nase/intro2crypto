067ade8748a482e6246cf3b8b8cee63caa39d160
Nicole Nasengo <nasengonicole@gmail.com>
2024-09-05 13:51:39 +0300

    base 64

--- great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
+++ great_snakes_35381fca29d68d8f3f25c9fa0a9026fb.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-
 import sys
+import base64
 # import this
-
 if sys.version_info.major == 2:
     print("You are running Python 2, which is no longer supported. Please update to Python 3.")
 
@@ -12,6 +11,9 @@ flag = "".join(chr(value) for value in char)
 print("Here is your flag:"+flag)
 
 flag2 = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+print( "your flag is: " + bytes.fromhex(flag2).decode())
 
-print( "your flag is: " + bytes.fromhex(flag2).decode() )
+flag3 = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+flag3_1 = bytes.fromhex(flag3)
+print("your flag is: " + base64.b64encode(flag3_1).decode())
 
